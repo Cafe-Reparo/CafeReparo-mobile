183d677a431f2d20fc3dd9d7acf3c8bb86c9c814
ALenckulh <127156754+ALenckulh@users.noreply.github.com>
2024-11-29 17:07:51 -0300

    style: searchpage

--- lib/pages/search_page.dart
+++ lib/pages/search_page.dart
@@ -37,113 +37,111 @@ class _SearchPageState extends State<SearchPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: const Header(),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 32),
-        child: Column(
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                SearchField(
-                  onPressed: () {
-                    setState(() {
-                      // Lógica opcional ao interagir com o campo
-                    });
-                  },
-                  width: 300,
-                  controller: searchController,
-                ),
-                const SizedBox(width: 8),
-                IconPurpleButton(
-                  onPressed: () {
-                    // Navegação interna para atualizar a pesquisa
-                    String newQuery = searchController.text;
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => SearchPage(query: newQuery),
-                      ),
-                    );
-                  },
-                  icon: PhosphorIcons.arrowRight,
-                ),
-              ],
-            ),
-            const SizedBox(height: 32),
-            Expanded(
-              child: ListView.builder(
-                padding: const EdgeInsets.all(0),
-                itemCount: services.length,
-                itemBuilder: (context, index) {
-                  final service = services[index];
-                  return CustomCard(
-                    child: Padding(
-                      padding: const EdgeInsets.all(16.0),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            service['title'],
-                            style: Theme.of(context)
-                                .primaryTextTheme
-                                .titleSmall
-                                ?.copyWith(
-                                  color: MyColors.primary550,
-                                ),
-                          ),
-                          Text(
-                            service['location'],
-                            style: Theme.of(context)
-                                .primaryTextTheme
-                                .bodySmall
-                                ?.copyWith(
-                                  color: MyColors.white100,
-                                ),
-                          ),
-                          const SizedBox(height: 4),
-                          Text(
-                            service['description'],
-                            style: Theme.of(context)
-                                .primaryTextTheme
-                                .labelMedium
-                                ?.copyWith(
-                                  color: MyColors.secondary100,
-                                ),
-                            maxLines: 3,
-                            overflow: TextOverflow.ellipsis,
-                          ),
-                          const SizedBox(height: 16),
-                          SingleChildScrollView(
-                            scrollDirection: Axis.horizontal,
-                            child: Row(
-                              children: List.generate(
-                                service['expertise'].length,
-                                (chipIndex) => Padding(
-                                  padding: const EdgeInsets.only(right: 8),
-                                  child: Chip(
-                                    label: Text(
-                                      service['expertise'][chipIndex],
-                                      style: Theme.of(context)
-                                          .primaryTextTheme
-                                          .labelLarge
-                                          ?.copyWith(
-                                            color: MyColors.secondary200,
-                                          ),
-                                    ),
+      body: Column(
+        children: [
+          const SizedBox(height: 32),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SearchField(
+                onPressed: () {
+                  setState(() {
+                    // Lógica opcional ao interagir com o campo
+                  });
+                },
+                width: 300,
+                controller: searchController,
+              ),
+              const SizedBox(width: 8),
+              IconPurpleButton(
+                onPressed: () {
+                  // Navegação interna para atualizar a pesquisa
+                  String newQuery = searchController.text;
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => SearchPage(query: newQuery),
+                    ),
+                  );
+                },
+                icon: PhosphorIcons.arrowRight,
+              ),
+            ],
+          ),
+          const SizedBox(height: 12),
+          Expanded(
+            child: ListView.builder(
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 12),
+              itemCount: services.length,
+              itemBuilder: (context, index) {
+                final service = services[index];
+                return CustomCard(
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          service['title'],
+                          style: Theme.of(context)
+                              .primaryTextTheme
+                              .titleSmall
+                              ?.copyWith(
+                                color: MyColors.primary550,
+                              ),
+                        ),
+                        Text(
+                          service['location'],
+                          style: Theme.of(context)
+                              .primaryTextTheme
+                              .bodySmall
+                              ?.copyWith(
+                                color: MyColors.white100,
+                              ),
+                        ),
+                        const SizedBox(height: 4),
+                        Text(
+                          service['description'],
+                          style: Theme.of(context)
+                              .primaryTextTheme
+                              .labelMedium
+                              ?.copyWith(
+                                color: MyColors.secondary100,
+                              ),
+                          maxLines: 3,
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                        const SizedBox(height: 16),
+                        SingleChildScrollView(
+                          scrollDirection: Axis.horizontal,
+                          child: Row(
+                            children: List.generate(
+                              service['expertise'].length,
+                              (chipIndex) => Padding(
+                                padding: const EdgeInsets.only(right: 8),
+                                child: Chip(
+                                  label: Text(
+                                    service['expertise'][chipIndex],
+                                    style: Theme.of(context)
+                                        .primaryTextTheme
+                                        .labelLarge
+                                        ?.copyWith(
+                                          color: MyColors.secondary200,
+                                        ),
                                   ),
                                 ),
                               ),
                             ),
                           ),
-                        ],
-                      ),
+                        ),
+                      ],
                     ),
-                  );
-                },
-              ),
+                  ),
+                );
+              },
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
